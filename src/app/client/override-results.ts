acaf1cdf154e82dd28e526ecd7538854d1efa306
varun-tavisca <varun.srinivas@tavisca.com>
2021-01-07 16:29:33 +0530

    saving work

--- src/app/client/override-results.ts
+++ src/app/client/override-results.ts
@@ -1,26 +1,25 @@
-import { Hooks, OrxeHook } from "@orxe-sdk/hooks";
+import { Hooks, OrxeHook } from '@orxe-sdk/hooks';
 
 @OrxeHook({
-  selector: "app-my-search",
-  method: "getResults",
-  executeOriginal: false,
+    selector: 'app-my-search',
+    method: 'getResults',
+    executeOriginal: true
 })
 export class OverrideResults implements Hooks {
-  before() {
-    console.log("before results override");
-    return new Promise<any>((resolve, reject) => {
-      setTimeout(() => {
-        resolve();
-      }, 2000);
-    });
-  }
+    before() {
+        console.log('before override');
+        return new Promise<any>((resolve, reject) => {
+            setTimeout(() => {
+                resolve();
+            }, 2000);
+        });
+    }
 
-  override() {
-    return [{ a: 1 }];
-  }
+    override() {
+        return [{ a: 1 }];
+    }
 
-  after(): void {
-    console.log("after results override");
-    // throw new Error("Method not implemented.");
-  }
+    after(): void {
+        console.log('after override');
+    }
 }
--- src/app/my-search/my-search.component.ts
+++ src/app/my-search/my-search.component.ts
@@ -1,44 +1,45 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnDestroy, OnInit } from '@angular/core';
 
-import { Hookable } from "@orxe-sdk/hooks";
+import { Hookable } from '@orxe-sdk/hooks';
 
 @Component({
-  selector: "app-my-search",
-  templateUrl: "./my-search.component.html",
-  styleUrls: ["./my-search.component.scss"],
+    selector: 'app-my-search',
+    templateUrl: './my-search.component.html',
+    styleUrls: ['./my-search.component.scss']
 })
 @Hookable({
-  selector: "app-my-search",
-  hooks: ["ngOnInit", "getResults"],
+    selector: 'app-my-search',
+    hooks: ['ngOnInit', 'getResults']
 })
 export class MySearchComponent implements OnInit {
-  results: any[];
-  constructor() {}
+    results: any[];
+    constructor() {}
 
-  ngOnInit() {
-    console.log("Initing....");
-    // this.results = this.getResults();
-  }
+    ngOnInit() {
+        console.log('Initing....');
+        alert('Inited');
+    }
 
-  getResults() {
-    console.log("Results...");
-    return [
-      {
-        id: 1,
-        name: "A",
-      },
-      {
-        id: 2,
-        name: "B",
-      },
-      {
-        id: 3,
-        name: "C",
-      },
-      {
-        id: 4,
-        name: "D",
-      },
-    ];
-  }
+    getResults() {
+        console.log('Results...');
+        alert('Results Called!');
+        return [
+            {
+                id: 1,
+                name: 'A'
+            },
+            {
+                id: 2,
+                name: 'B'
+            },
+            {
+                id: 3,
+                name: 'C'
+            },
+            {
+                id: 4,
+                name: 'D'
+            }
+        ];
+    }
 }
